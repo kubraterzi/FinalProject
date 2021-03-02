005bc4fde9ebc9703e0a78e01df00c7f496b24e2
Kübra Terzi <kubraterzi0303@gmail.com>
2021-03-02 16:15:12 +0300

    Updated - FinalProject Added - Security Templates(Json Web Token)

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JWT;
+
+namespace Business.Abstract
+{
+    public interface AuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto,string password);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult Exists(string password);
+        IDataResult<AccesToken> CreateAccessToken(User user);
+
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace DataAccess.Concrete
+{
+    public class EfUserDal : EfEntityRepositoryBase<User,NorthwindContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                    join userOperationClaim in context.UserOperationClaims 
+                        on operationClaim.Id equals userOperationClaim.Id
+                    where userOperationClaim.UserId == user.Id
+                    select new OperationClaim
+                    {
+                        Id = userOperationClaim.OperationClaimId,
+                        Name = operationClaim.Name
+                    };
+
+                return result.ToList();
+            }
+            
+        }
+    }
+}
